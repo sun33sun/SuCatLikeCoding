94f87f6dd4e045e61742f13bd4a45444af32fcfa
sun33sun <1255317579@qq.com>
2023-06-12 20:00:35 +0800

    升级至2021.3.23f1c1

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+	[SerializeField] Transform pointPrefab;
+	[SerializeField] int resolution;
+	void Start()
+	{
+		float step = 2f / resolution;
+		var pos = Vector3.zero;
+		var scale = Vector3.one * step;
+		for (int i = 0; i < resolution; i++)
+		{
+			Transform point = Instantiate(pointPrefab);
+			pos.x = (i + 0.5f) * step - 1f;
+			pos.y = Fun(pos.x);
+			point.SetParent(transform, false);
+			point.localScale = scale;
+			point.localPosition = pos;
+		}
+	}
+
+	float Fun(float value)
+	{
+		return Mathf.Pow(value, 3);
+	}
+}
+
---
+++ Assets/Shaders/Point Surface.shader
@@ -0,0 +1,28 @@
+﻿Shader "Graph/Point Surface"
+{
+	Properties
+	{
+		_Smoothness("Smoothness",Range(0,1)) = 0.5
+	}
+		SubShader
+	{
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows
+		#pragma target 3.0
+
+        float _Smoothness;
+
+		struct Input
+		{
+			float3 worldPos;
+		};
+		
+		void surf(Input input, inout SurfaceOutputStandard surface)
+		{
+			surface.Smoothness = _Smoothness;
+			surface.Albedo.rg = input.worldPos.xy * 0.5 + 0.5;
+		}
+		ENDCG
+	}
+		FallBack "Diffuse"
+}
